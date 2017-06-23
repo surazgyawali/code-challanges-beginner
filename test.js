14376ee4ec9c3db003b1be8675d8c5d32e525906
suraj gyawali <surazgyawali@gmail.com>
2017-06-23 09:09:41 +0545

    .gitignore updated.

--- test.js
+++
@@ -1,7 +0,0 @@
-
-//Just a empty file to test while practising 
-
-//Suppose u have bag taht can hold 10kg eggs.
-//There are 5 eggs with 2,6,4,3,1 kg each with nutrition value of 10,20,30,40,50.
-//like (2,10),(6,20),(4,30),(3,40)(1,50).
-//Now write program to get maximum nutrient and maximum weight in the bag.
